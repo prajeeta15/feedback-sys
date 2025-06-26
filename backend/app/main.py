564424b29bd5d73a318b23d5135d5be1e6333c64
prajeeta pal <96904203+prajeeta15@users.noreply.github.com>
2025-06-26 15:44:27 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routes import auth, users, feedback
+from app.routes import users, feedback
+from app import auth
 
 Base.metadata.create_all(bind=engine)
 
